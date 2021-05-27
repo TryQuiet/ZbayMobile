9521d7296527051f472a6322d953d3fbfe087d59
Wiktor <wiktor.sieprawski@rumblefish.pl>
2021-05-27 13:45:06 +0200

    Logic moved from containers to sagas. Setup .env

--- src/screens/Main/Main.screen.tsx
+++ src/screens/Main/Main.screen.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useEffect} from 'react';
+import {Config} from 'react-native-config';
 import {Text, ScrollView} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {publicChannelsSelectors} from '../../store/publicChannels/publicChannels.selectors';
@@ -11,16 +12,9 @@ export const MainScreen: FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(publicChannelsActions.getPublicChannels());
-  }, [dispatch]);
-
-  useEffect(() => {
     let Zbay: IChannelInfo | null = null;
     for (var i = 0; i < channels.length; i++) {
-      if (
-        channels[i].address ===
-        'zs10zkaj29rcev9qd5xeuzck4ly5q64kzf6m6h9nfajwcvm8m2vnjmvtqgr0mzfjywswwkwke68t00'
-      ) {
+      if (channels[i].address === Config.PUBLIC_CHANNEL_ADDRESS) {
         Zbay = channels[i];
         break;
       }
