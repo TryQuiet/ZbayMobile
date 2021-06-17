8303d41474226dd6c1e2a1093e960bebd6468283
Wiktor <wiktor.sieprawski@rumblefish.pl>
2021-06-17 17:47:51 +0200

    Tests fixed

--- src/store/init/doOnRestore/doOnRestore.saga.test.ts
+++ src/store/init/doOnRestore/doOnRestore.saga.test.ts
@@ -1,15 +1,23 @@
-import { TestApi, testSaga } from 'redux-saga-test-plan';
+import { combineReducers } from 'redux';
+import { expectSaga } from 'redux-saga-test-plan';
+import { select } from 'redux-saga-test-plan/matchers';
+import { ScreenNames } from '../../../const/ScreenNames.enum';
+import { replaceScreen } from '../../../utils/functions/replaceScreen/replaceScreen';
+import { StoreKeys } from '../../store.keys';
+import { initSelectors } from '../init.selectors';
+import { initReducer, InitState } from '../init.slice';
 
 import { doOnRestoreSaga } from './doOnRestore.saga';
 
 describe('doOnRestoreSaga', () => {
-  const saga: TestApi = testSaga(doOnRestoreSaga);
-
-  beforeEach(() => {
-    saga.restart();
-  });
-
-  test('should be defined', () => {
-    saga.next().isDone();
+  test('replace screen on restore', () => {
+    expectSaga(doOnRestoreSaga)
+      .withReducer(combineReducers({ [StoreKeys.Init]: initReducer }), {
+        [StoreKeys.Init]: {
+          ...new InitState(),
+        },
+      })
+      .provide([[select(initSelectors.isRestored), true]])
+      .call(replaceScreen, ScreenNames.MainScreen);
   });
 });
