265dbe58c27e08f1e3ce820e62bd9a2f4329a26b
Wiktor <wiktor.sieprawski@rumblefish.pl>
2021-07-12 12:25:28 +0200

    Error handling

--- src/store/messages/sendMessage/sendMessage.saga.ts
+++ src/store/messages/sendMessage/sendMessage.saga.ts
@@ -13,6 +13,11 @@ import { publicChannelsSelectors } from '../../publicChannels/publicChannels.sel
 import { messagesActions } from '../messages.slice';
 import { MessageTypes } from '../const/messageTypes';
 import { arrayBufferToString } from 'pvutils';
+import { navigateTo } from '../../../utils/functions/navigateTo/navigateTo';
+import { ScreenNames } from '../../../const/ScreenNames.enum';
+import { Dispatch } from 'react';
+import { appImages } from '../../../../assets';
+import { replaceScreen } from '../../../utils/functions/replaceScreen/replaceScreen';
 
 export function* sendMessageSaga(
   socket: Socket,
@@ -22,13 +27,29 @@ export function* sendMessageSaga(
 ): Generator {
   const csr = yield* select(identitySelectors.userCsr);
   if (!csr) {
-    // Provide error handling
+    yield* call(navigateTo, ScreenNames.ErrorScreen, {
+      onPress: (_dispatch: Dispatch<any>) => {
+        replaceScreen(ScreenNames.MainScreen);
+      },
+      icon: appImages.zbay_icon,
+      title: 'Error',
+      message:
+        "User secrets are missing. You're not able to send messages without it. Try to restart the app or install it again.",
+    });
     return;
   }
 
   const certificate = yield* select(identitySelectors.userCertificate);
   if (!certificate) {
-    // Provide error handling
+    yield* call(navigateTo, ScreenNames.ErrorScreen, {
+      onPress: (_dispatch: Dispatch<any>) => {
+        replaceScreen(ScreenNames.MainScreen);
+      },
+      icon: appImages.zbay_icon,
+      title: 'Error',
+      message:
+        'User certificate is missing. You need it to let the others know which messages was sent by you. Try to register your username again.',
+    });
     return;
   }
 
